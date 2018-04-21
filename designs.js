0fb8ec1314196a6bc38ee430bb837e4ef5b45305
Michael Valentin <Michael.Valentin1136@gmail.com>
2018-04-21 15:42:19 -0400

    Version 0.1.03
    Adds ability to erase

--- designs.js
+++ designs.js
@@ -16,16 +16,46 @@ function makeGrid() {
     {
         $("table").append("<tr></tr>");
     }
-    for (let y = 1; y <= M; y++) //Add Data
+    for (let y = 1; y <= M; y++) //Add Cols
     {
         $("tr").append("<td></td>");
     }
 
 
     $("table").on("click", "td", function () {
+
         let colorPick = $("#colorPicker").val();
-        $(this).css("background-color", colorPick);
+        console.log(colorPick);
+        console.log(($(this).css("background-color")));
+        if ($(this).css("background-color") == colorPick) {
+            $(this).css("background-color","#ffff");
+        } else {
+            $(this).css("background-color", colorPick);
+        }
+
     })
 
 
 }
+$.cssHooks.backgroundColor = {
+    get: function (elem) {
+        if (elem.currentStyle)
+            var bg = elem.currentStyle["backgroundColor"];
+        else if (window.getComputedStyle)
+            var bg = document.defaultView.getComputedStyle(elem,
+                null).getPropertyValue("background-color");
+        if (bg.search('rgba') > -1) {
+            return '#00ffffff';
+        } else {
+            if (bg.search('rgb') == -1) {
+                return bg;
+            } else {
+                bg = bg.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+                function hex(x) {
+                    return ("0" + parseInt(x).toString(16)).slice(-2);
+                }
+                return "#" + hex(bg[1]) + hex(bg[2]) + hex(bg[3]);
+            }
+        }
+    }
+};
