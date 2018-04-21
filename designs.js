0d816a63d788eabecd48bf3518e245959556b564
Michael Valentin <Michael.Valentin1136@gmail.com>
2018-04-21 14:03:53 -0400

    Version 0.1.01
    -Organizes the document.
    -Added info to Readme.

--- designs.js
+++ designs.js
@@ -1,37 +1,31 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 $(document).ready(function () {
-    let canvas= $("#pixelCanvas");
+    let canvas = $("#pixelCanvas");
     $("#sizePicker").submit(function (event) {
-            event.preventDefault();
-            canvas.html("");
-            makeGrid();
-        }
-    );
+        event.preventDefault();
+        canvas.html("");
+        makeGrid();
+    });
 });
 
 function makeGrid() {
     let N = $("#inputHeight").val();
     let M = $("#inputWeight").val();
-    
-    
-    for (let x = 1; x<= N; x++)//Adds Row
+
+
+    for (let x = 1; x <= N; x++) //Adds Row
     {
         $("table").append("<tr></tr>");
     }
-    for (let y = 1; y<= M; y++) //Add Data
+    for (let y = 1; y <= M; y++) //Add Data
     {
         $("tr").append("<td></td>");
     }
-    
 
-$("table").on ("click","td",function (){
-     let colorPick=$("#colorPicker").val();
-     $(this).css("background-color",colorPick);
- })
 
-   
+    $("table").on("click", "td", function () {
+        let colorPick = $("#colorPicker").val();
+        $(this).css("background-color", colorPick);
+    })
+
+
 }
